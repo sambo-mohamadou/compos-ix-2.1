100bf61cb61791bde2753b70687696abf643cd50
sambo-mohamadou <sambo.m.bachirou@gmail.com>
2023-11-13 14:40:10 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Starter Code</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+import withMT from "@material-tailwind/react/utils/withMT";
+
+module.exports = withMT({
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'googleGreen': '#34A853',
+      'googleRed': '#EA4335',
+      'googleYellow': '#FBBC05',
+      'googleBlue': '#4285F4',
+      'lightBlue': '#E2EBF9',
+    },
+    extend: {},
+  },
+  plugins: [],
+})
+
+
